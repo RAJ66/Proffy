d6c88b4b3d578e404c1b259dfba201e47a6255e6
Vitor <vitorbarbosa1998@hotmail.com>
2020-08-06 14:51:19 +0100

    feat: added study page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 
+import backIcon from "../../assets/images/icons/back.svg";
+import logoIcon from "../../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+
 // import { Container } from './styles';
 
 function TeacherList() {
-  return <h1>Teacher</h1>;
+  return (
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Back" />
+          </Link>
+          <img src={logoIcon} alt="Logo" />
+        </div>
+        <div className="header-content">
+          <strong>This are the proffys available.</strong>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default TeacherList;
