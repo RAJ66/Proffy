3e877d333e5ba685dfc29526c8a4ab912adb7d6d
Vitor <vitorbarbosa1998@hotmail.com>
2020-08-07 17:53:18 +0100

    feat: added header component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader";
 
-// import { Container } from './styles';
+import "./styles.css";
 
 function TeacherForm() {
-  return <h1>Teacher Form</h1>;
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="How awesome is that you want to teach." />
+    </div>
+  );
 }
 
 export default TeacherForm;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,25 +1,12 @@
 import React from "react";
 
-import backIcon from "../../assets/images/icons/back.svg";
-import logoIcon from "../../assets/images/logo.svg";
-import { Link } from "react-router-dom";
-
 import "./styles.css";
+import PageHeader from "../../components/PageHeader";
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Back" />
-          </Link>
-          <img src={logoIcon} alt="Logo" />
-        </div>
-        <div className="header-content">
-          <strong>This are the proffys available.</strong>
-        </div>
-      </header>
+      <PageHeader title="This are the proffys available." />
     </div>
   );
 }
