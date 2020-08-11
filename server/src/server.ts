0d28f777decf0af2d31ff71c05fe8a2b5ed0e774
Vitor <vitorbarbosa1998@hotmail.com>
2020-08-11 11:03:17 +0100

    fix: small bug fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  return response.json([{ User: "Vitor" }]);
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json([{ massage: "Hello World" }]);
 });
 
 app.listen(3333);
